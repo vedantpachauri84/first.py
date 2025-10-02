9636a12052006a0abec80d3772d8904db1b49390
vedant pachauri <vedantpachauri84@gmail.com>
2025-10-02 19:12:58 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -1,1 +1,7 @@
-print("value")
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+print("addition is equal to ",+ a+b)
+print("substraction is equal to\t", +a-b)
+print("multiplication is equal to\t",+a*b)
+print("divide is equal to\t ",+a/b)
+          
